39b0089fa6ceda7a323782bbae55e17e7211735a
salman zafar <salmanzafar949@gmail.com>
2017-08-22 16:33:06 +0500

    Added Scout in user.php

--- app/User.php
+++ app/User.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App;
-
+use Laravel\Scout\Searchable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    
+    use Searchable;
+    
     use Notifiable;
+
 
     /**
      * The attributes that are mass assignable.
